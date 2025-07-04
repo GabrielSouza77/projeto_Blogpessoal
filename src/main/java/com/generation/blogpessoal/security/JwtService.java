e739eb7810013473b41cf41bbe959c2810c2384d
Gabriel Souza <souzagabriels@hotmail.com>
2025-07-04 00:41:33 -0300

    :)

--- src/main/java/com/generation/blogpessoal/security/JwtService.java
+++ src/main/java/com/generation/blogpessoal/security/JwtService.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
@@ -18,10 +19,13 @@ import io.jsonwebtoken.security.Keys;
 @Component
 public class JwtService {
 
-	public static final String SECRET = System.getenv("JWT_SECRET");
-
+	@Value("${jwt.secret}")
+	private String secret;
+	
+	private static final int EXPIRATION = 1000 * 60 * 60;
+	
 	private Key getSignKey() {
-		byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+		byte[] keyBytes = Decoders.BASE64.decode(secret);
 		return Keys.hmacShaKeyFor(keyBytes);
 	}
 
